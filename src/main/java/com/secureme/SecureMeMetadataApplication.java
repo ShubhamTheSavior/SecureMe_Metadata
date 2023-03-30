92c04a3797c8e7b065309d891bf9c078714128fb
ShubhamTheSavior <shubham.gadekar990@gmail.com>
2023-03-30 22:57:57 +0530

    secureme metdata

---
+++ src/main/java/com/secureme/SecureMeMetadataApplication.java
@@ -0,0 +1,15 @@
+package com.secureme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SecureMeMetadataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureMeMetadataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secureme/SecureMeMetadataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureMeMetadataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
